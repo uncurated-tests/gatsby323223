e1a0f86c137e10329e3cb063e64bd8a2c09ef975
Leo Lamprecht <mindrun@icloud.com>
2020-08-08 19:26:17 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ function Index() {
         </a>{' '}
         is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
         directorieadadss, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
+        which contains a servesadsdaadrless{' '}
         <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
         <a href="/api/date">
           <code>api/date</code> for the Date API with Node.js (TypeScript)
